243a414de99be194bfddef8f11f84f0860c3665b
Anton <bogdan.anton@metroitpartner.com>
2022-01-02 21:05:29 +0200

    First Commit

---
+++ backend/src/main/java/com/youlearn/youlearn/YoulearnApplication.java
@@ -0,0 +1,13 @@
+package com.youlearn.youlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoulearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoulearnApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/youlearn/youlearn/YoulearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youlearn.youlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoulearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
