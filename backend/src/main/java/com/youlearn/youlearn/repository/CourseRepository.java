920bb71932b4db12778b7e625dd42fc579e0eacb
Anton Bogdan <antonbogdan19@stud.ase.ro>
2022-03-27 20:44:36 +0300

    updated endpoint which checks if course does exist

--- backend/src/main/java/com/youlearn/youlearn/repository/CourseRepository.java
+++ backend/src/main/java/com/youlearn/youlearn/repository/CourseRepository.java
@@ -2,11 +2,13 @@ package com.youlearn.youlearn.repository;
 
 import com.youlearn.youlearn.model.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
+    @Query("SELECT c FROM Course c WHERE c.courseYoutubeId like concat('%', ?1, '%')")
     Optional<Course> findByCourseYoutubeId(String courseYoutubeId);
 }
